2a332786863637da465cc36441490cb19aafe59a
Timothy <tkkibet13@gmail.com>
2022-08-23 09:28:23 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.getElementById("submit").addEventListener("sees", function(event){
+    event.preventDefault()
+  });
 });
