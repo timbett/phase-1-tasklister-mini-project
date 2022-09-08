131009e422331638a3e842326c4c84c90fd20293
Timothy <tkkibet13@gmail.com>
2022-09-09 00:30:21 +0300

    completed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-  document.getElementById("submit").addEventListener("sees", function(event){
-    event.preventDefault()
-  });
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    newTask(e.target.new_task.value)
+  })
 });
+
+function newTask(task) {
+  let ul = document.getElementById('tasks')
+  let li = document.createElement('li')
+  ul.appendChild(li)
+  li.innerHTML = `${task}  `;
+  let btn = document.createElement('button');
+  btn.addEventListener('click', removeToDoList)
+  btn.innerHTML = 'X'
+  li.appendChild(btn)
+
+
+}
+
+function removeToDoList(e) {
+    e.target.parentNode.remove()
+}
